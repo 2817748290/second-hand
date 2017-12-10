b412de4cafb4fe2bd0ae4c605fddd6f65a2c4590
YannPro <849723885@qq.com>
2017-12-10 18:44:05 +0800

    更换vue-admin模版

---
+++ bookmanager-vue/admin/src/api/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+let base = '';
+
+export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
+
+// export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
+export const getUserList = params => { return axios.post(`${base}/api/user/getSearchUser`, { params: params }); };
+
+export const getUserListPage = params => { return axios.post(`${base}/api/user/getSearchUser`, { params: params }); };
+
+export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+
+export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+
+export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
