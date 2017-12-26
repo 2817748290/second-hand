e3b29b895ba828d73c00e41f9f3d7c6eaa640c38
2817748290 <2817748290@qq.com>
2017-12-26 10:00:25 +0800

    bug

--- bookmanager-vue/admin/src/pages/Login.vue
+++ bookmanager-vue/admin/src/pages/Login.vue
@@ -1,18 +1,22 @@
 <template>
+<div>
+  <div class="bg"></div>
   <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm card-box loginform">
-    <h3 class="title">系统登录</h3>
+    <h2 class="title">系统登录</h2>
     <el-form-item prop="account">
       <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
     </el-form-item>
     <el-form-item prop="password">
       <el-input type="password" v-model="ruleForm2.password" auto-complete="off" placeholder="密码"></el-input>
     </el-form-item>
-    <el-checkbox v-model="checked" checked style="margin:0px 0px 35px 0px;">记住密码</el-checkbox>
-<el-form-item style="width:100%;">
-<el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
-<!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
-</el-form-item>
-</el-form>
+      <el-checkbox v-model="checked" checked style="margin:0px 0px 35px 0px;">记住密码</el-checkbox>
+    <el-form-item style="width:100%;">
+    <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
+    <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+    </el-form-item>
+  </el-form>
+</div>
+  
 </template>
 
 <script>
@@ -23,7 +27,7 @@
       return {
         logining: false,
         ruleForm2: {
-          account: 'admin1',
+          account: 'zongyuhui',
           password: '123456'
         },
         rules2: {
@@ -59,11 +63,11 @@
               if (data.status !== 1) {
                 this.$notify({
                   title: '错误',
-                  message: msg,
+                  message: '账号或密码错误',
                   type: 'error'
                 });
               } else {
-                localStorage.setItem('user', JSON.stringify({'username':loginParams.username}));
+                localStorage.setItem('user', JSON.stringify({'username':loginParams.username,'userId':data.result}));
                 if (this.$route.query.redirect) {
                   this.$router.push({ path: this.$route.query.redirect });
                 } else {
@@ -94,10 +98,12 @@
     margin: 180px auto;
     width: 400px;
     border: 2px solid #8492A6;
+    opacity: .8;
   }
   
   .title {
-    margin: 0px auto 40px auto;
+    font-size: 25px;
+    margin: 0px auto 20px auto;
     text-align: center;
     color: #505458;
   }
@@ -105,5 +111,16 @@
   .loginform {
     width: 350px;
     padding: 35px 35px 15px 35px;
+  }
+
+  .bg{
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: url("../../static/bg.jpg");
+    background-size: cover;
+    z-index: -50;
   }
 </style>
